bdae2afcebb8c9cb9651d9ba56b7db28c44f9168
AridoshikaZu103 <soorajidpcse@gmail.com>
2026-04-27 01:58:20 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+      // Allow all origins for easy deployment
+      callback(null, true);
+    },
     credentials: true,
   })
 );
